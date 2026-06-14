dc0f9124c749d898d1e9bace5258d1ed525325be
charan199189 <hnrr6929@gmail.com>
2026-06-14 20:00:39 +0530

    Attendance website update

---
+++ SD.js
@@ -0,0 +1,592 @@
+  var students = {
+
+"25H71A05D3":{
+name:"Akula Rajini",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05D4":{
+name:"Allu Harsha Vardhan",
+course:"B.Tech",
+year:"1st Year",
+semester:"I",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05D5":{
+name:"Avula Lakshmi Narayana",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05D6":{
+name:"Badisa Venkata Lakshmi Siva Kumari",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05D7":{
+name:"Balsu Nandhini",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05D8":{
+name:"Banavathu Maheswari Bai",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05D9":{
+name:"Bandaru Vidyasagar",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E0":{
+name:"Bandi Uma",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E1":{
+name:"Bellapu Kartheek",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E2":{
+name:"Bhukya Hima Sagar",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E3":{
+name:"Biyyala Bhavani",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E4":{
+name:"Challagonda Venu",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E5":{
+name:"Chegondi Suresh",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E6":{
+name:"Chimmini Naveen",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E7":{
+name:"Chintapalli Lakshmi Durga Saranya",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E8":{
+name:"Dunna Pujitha",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05E9":{
+name:"Eerla Bindusri",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F0":{
+name:"Erla Manikanta",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F1":{
+name:"Gollapalli Siva Gopi Chand",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F2":{
+name:"Gudepu Sandeep",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F3":{
+name:"Gummadivelli Prajwal",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F4":{
+name:"Gunji Ganga Bhavani",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F5":{
+name:"Guttula Lakshmi Teja Hari Kumar",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F6":{
+name:"Ilapogu Babu",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F7":{
+name:"Jetipiti Ramcharan",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F8":{
+name:"Kaithepalli Karthikeya",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05F9":{
+name:"Kasaragadda Sai Mahathi",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G0":{
+name:"Katevarapu Anu",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G1":{
+name:"Kodali Krupa Niharika",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G2":{
+name:"Kona Jyothi Krishna",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G3":{
+name:"Korata Lohith Venkata Sai Raju",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G4":{
+name:"Kotha Yeswanth Gopal Krishna",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G6":{
+name:"Mandala Bhavana",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G7":{
+name:"Mandru Rohith",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G8":{
+name:"Martha Hemanth",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05G9":{
+name:"Matta Thiru Ranganadh",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H0":{
+name:"Midde Vasanthi",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H1":{
+name:"Miriyala Praveen",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H2":{
+name:"Mohammad Husna",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H4":{
+name:"Narukulla Deepak Chowdary",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H5":{
+name:"Pagadala Hemanth Kumar",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H6":{
+name:"Palanki Nikhitha",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H7":{
+name:"Pamarthi Jaswanthi",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H8":{
+name:"Pendli Abhishay",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05H9":{
+name:"Penugonda Sri Mouli Krishna",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I0":{
+name:"Raavi Keerthi Priya[CR]",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I1":{
+name:"Raavi Shanmukha Priya",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I2":{
+name:"Rajaboena Komali Sree",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I3":{
+name:"Ravuri Rama Harinadh",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I4":{
+name:"Rayala Lokesh",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I5":{
+name:"Sareddy Pranav Kumar Reddy",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I6":{
+name:"Sayyad Farjana",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I7":{
+name:"Shaik Dilshad Begum",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I8":{
+name:"Shaik Karishma",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05I9":{
+name:"Shaik Nowmon",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J0":{
+name:"Shaik Rizwan",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J1":{
+name:"Shaik Rizwana",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J2":{
+name:"Sunke Lakshmi Deepa",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J3":{
+name:"Tripurani Chaitanya Lakshmi",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J4":{
+name:"Thangirala Ravi Kumar",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J5":{
+name:"Yaddanapudi Gowthami",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J6":{
+name:"Yadlapalli Naga Chaithanya",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J7":{
+name:"Yalamanchili Tejasri",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+"25H71A05J8":{
+name:"Yeruva Venkata Rakesh",
+course:"B.Tech",
+year:"1st Year",
+semester:"II",
+branch:"CSE",
+section:"C"
+},
+
+
+
+/* DETAINED */
+"25H71A05G5":{
+name:"Lanka Sindhu",
+status:"detained"
+},
+
+"25H71A05H3":{
+name:"Nagaraju Manikanta Madhav",
+status:"detained"
+}
+
+};  
